78bee66fe2732b77f98a81d0c380b3123e818732
2년 차 개발자 이재영입니다 <148510209+LeeJaeYoung0706@users.noreply.github.com>
2024-04-01 21:58:26 +0900

    Add files via upload

---
+++ src/test/java/ToyProject/NewDy/REST_API/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ToyProject.NewDy.REST_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
