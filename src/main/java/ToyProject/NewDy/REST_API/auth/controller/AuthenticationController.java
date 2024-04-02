06a64a41c681e09b1237a1af7d6252fdc8c0f7d7
jaesoon3737 <wodud4019@gmail.com>
2024-04-02 10:46:16 +0900

    스웨거 의존성 주입 및 Config

--- src/main/java/ToyProject/NewDy/REST_API/auth/controller/AuthenticationController.java
+++ src/main/java/ToyProject/NewDy/REST_API/auth/controller/AuthenticationController.java
@@ -1,13 +1,14 @@
 package ToyProject.NewDy.REST_API.auth.controller;
 
 
+import ToyProject.NewDy.REST_API.auth.swagger.AuthenticationControllerSwagger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-public class AuthenticationController {
+public class AuthenticationController implements AuthenticationControllerSwagger {
 
     @GetMapping("/test")
     public String testRequest() {
---
+++ src/main/java/ToyProject/NewDy/REST_API/auth/swagger/AuthenticationControllerSwagger.java
@@ -0,0 +1,7 @@
+package ToyProject.NewDy.REST_API.auth.swagger;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "AuthenticationController", description = "인증 컨트롤러")
+public interface AuthenticationControllerSwagger {
+}
